375601d116f782c9d38fd0e5198c08ef9fc0cc6b
Cristobal <cris.beforus@hotmail.com>
2024-10-16 00:26:00 -0300

    Juntar todoo

--- duoc/src/app/pages/pages-routing.module.ts
+++ duoc/src/app/pages/pages-routing.module.ts
@@ -8,14 +8,16 @@ import { ClaseComponent } from './clase/clase.component';
 import { RegistroAsistenciaComponent } from './registro-asistencia/registro-asistencia.component';
 import { RegistrarComponent } from './registrar/registrar.component';
 
+import { authGuard } from '../guard/auth.guard';
+import { redirectIfAuthGuard } from '../guard/redirect-if-auth.guard';
 const routes: Routes = [
-  {path:'home', component: HomeComponent},
-  {path:'login', component: LoginComponent},
-  {path:'password',component:PasswordComponent},
-  {path:'home/perfil',component:PerfilComponent},
-  {path:'home/clase',component:ClaseComponent},
-  {path:'home/registro-asistencia',component:RegistroAsistenciaComponent},
-  {path: 'registrar', component:RegistrarComponent},
+  {path:'home', component: HomeComponent, canActivate: [authGuard]},
+  {path:'login', component: LoginComponent,canActivate: [redirectIfAuthGuard]},
+  {path:'password',component:PasswordComponent,canActivate: [redirectIfAuthGuard]},
+  {path:'home/perfil',component:PerfilComponent , canActivate: [authGuard]},
+  {path:'home/clase',component:ClaseComponent, canActivate: [authGuard]},
+  {path:'home/registro-asistencia',component:RegistroAsistenciaComponent, canActivate: [authGuard]},
+  {path: 'registrar', component:RegistrarComponent,canActivate: [redirectIfAuthGuard]},
   {path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirigir a 'login' si la ruta está vacía
   {path: '**', redirectTo: 'login', pathMatch: 'full' }, // Redirigir a 'login' para rutas no encontradas
 ];
